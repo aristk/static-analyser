b41760c01367e35576675eac29a565d6c47a0508
Arist <arist.kojevnikov@gmail.com>
2016-10-31 15:39:42 +0100

    A very simple static analyzer for very simple dynamic language.

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#include "node.h"
+extern NBlock* programBlock;
+extern int yyparse();
+extern FILE *yyin;
+
+int main(int argc, char **argv)
+{
+    if(argc != 2) {
+        printf("usage: ./staticAnalyzer filename\n");
+        exit(0);
+    }
+    FILE* file = fopen(argv[1],"r");
+    if(file == NULL) {
+        printf("couldn’t open %s\n", argv[1]);
+        exit(0);
+    }
+    yyin = file; // now flex reads from file
+    yyparse();
+    fclose(file);
+    std::cout << programBlock << std::endl;
+    return 0;
+}
